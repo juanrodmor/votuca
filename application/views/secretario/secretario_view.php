c59446cf31458666d0479e7e237bf4f615680960
inmabox <ibsantamaria96@gmail.com>
2019-11-13 20:02:00 +0100

    Planteando un footer

--- application/views/secretario/secretario_view.php
+++ application/views/secretario/secretario_view.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url(); ?>/assets/css/bootstrap-datepicker.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/css/behaviour/footer.css" rel="stylesheet">    
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
 
   </head>
@@ -100,6 +101,68 @@
     </table>
 
 </div>
+</div>
+
+  <footer class="footer">
+  <div class="container">
+      <div class="row">
+      <div class="col-sm-3">
+          <h4 class="title">Sumi</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit, libero a molestie consectetur, sapien elit lacinia mi.</p>
+          <ul class="social-icon">
+          <a href="#" class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+          <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
+          <a href="#" class="social"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+          </ul>
+          </div>
+      <div class="col-sm-3">
+          <h4 class="title">My Account</h4>
+          <span class="acount-icon">
+          <a href="#"><i class="fa fa-heart" aria-hidden="true"></i> Wish List</a>
+          <a href="#"><i class="fa fa-cart-plus" aria-hidden="true"></i> Cart</a>
+          <a href="#"><i class="fa fa-user" aria-hidden="true"></i> Profile</a>
+          <a href="#"><i class="fa fa-globe" aria-hidden="true"></i> Language</a>
+        </span>
+          </div>
+      <div class="col-sm-3">
+          <h4 class="title">Category</h4>
+          <div class="category">
+          <a href="#">men</a>
+          <a href="#">women</a>
+          <a href="#">boy</a>
+          <a href="#">girl</a>
+          <a href="#">bag</a>
+          <a href="#">teshart</a>
+          <a href="#">top</a>
+          <a href="#">shos</a>
+          <a href="#">glass</a>
+          <a href="#">kit</a>
+          <a href="#">baby dress</a>
+          <a href="#">kurti</a>
+          </div>
+          </div>
+      <div class="col-sm-3">
+          <h4 class="title">Payment Methods</h4>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          <ul class="payment">
+          <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-credit-card" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-cc-visa" aria-hidden="true"></i></a></li>
+          </ul>
+          </div>
+      </div>
+      <hr>
+
+      <div class="row text-center"> © 2019. Hecho por grupo 5 pinf.</div>
+      </div>
+
+
+  </footer>
+
 
 
     <!-- Bootstrap core JavaScript
