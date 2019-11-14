f78d00eef92dbe7859172de7fb20389a31ac6e7c
juancarlosld <56406256+juancarlosld@users.noreply.github.com>
2019-11-14 22:11:45 +0100

    actualizada bd con nuevos registros
    el usuario u55555555 tiene como contraseña usuario, al igual que el u00000000

--- votuca.sql
+++ votuca.sql
@@ -2,10 +2,10 @@
 -- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 14-11-2019 a las 10:13:31
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-11-2019 a las 21:56:07
 -- Versión del servidor: 10.4.8-MariaDB
--- Versión de PHP: 7.2.23
+-- Versión de PHP: 7.2.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -46,19 +46,19 @@ INSERT INTO `rol` (`Id`, `Nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `secretariosDelegados`
+-- Estructura de tabla para la tabla `secretariosdelegados`
 --
 
-CREATE TABLE `secretariosDelegados` (
+CREATE TABLE `secretariosdelegados` (
   `Id_Secretario` int(11) NOT NULL,
   `Id_Votacion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `secretariosDelegados`
+-- Volcado de datos para la tabla `secretariosdelegados`
 --
 
-INSERT INTO `secretariosDelegados` (`Id_Secretario`, `Id_Votacion`) VALUES
+INSERT INTO `secretariosdelegados` (`Id_Secretario`, `Id_Votacion`) VALUES
 (4, 3),
 (3, 3);
 
@@ -82,7 +82,8 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`Id`, `Id_Rol`, `NombreUsuario`, `Password`) VALUES
 (1, 1, 'u00000000', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.'),
 (2, 2, 'a00000000', '$2y$12$sZ9YHmBqYETwRKfIKGSUT.4ti4rlapaM5uYNj2M.tn21KxSGlytLG'),
-(3, 3, 'u12345678', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.');
+(3, 3, 'u12345678', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.'),
+(5, 1, 'u55555555', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.');
 
 -- --------------------------------------------------------
 
@@ -95,6 +96,16 @@ CREATE TABLE `usuario_votacion` (
   `Id_Votacion` int(32) NOT NULL,
   `Id_Voto` int(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_votacion`
+--
+
+INSERT INTO `usuario_votacion` (`Id_Usuario`, `Id_Votacion`, `Id_Voto`) VALUES
+(1, 2, 0),
+(1, 5, 2),
+(5, 3, 1),
+(5, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -116,11 +127,11 @@ CREATE TABLE `votacion` (
 --
 
 INSERT INTO `votacion` (`Id`, `Titulo`, `Descripcion`, `FechaInicio`, `FechaFinal`, `isDelected`) VALUES
-(1, 'Primera Votacion', 'Esta es la primera votacion de mi sistema', '2019-11-10', '2019-11-16', 0),
+(1, 'Primera Votacion', 'Esta es la primera votacion de mi sistema, y he sido modificada', '2019-11-10', '2019-11-16', 0),
 (2, 'Otra Votacion modificada', 'Soy otra votacion', '2019-11-24', '2019-11-27', 0),
 (3, 'otra jejejje ', 'fvfdbghggh', '2019-11-11', '2019-11-16', 0),
 (4, 'ME HE MODIFICADO', 'fvfdbghggh', '2019-11-11', '2019-11-17', 0),
-(7, 'no sé que hago con mi vida 2', 'Vota si este humilde señor deberia de comer macarrones o lentejas', '2019-11-11', '2019-11-13', 0),
+(5, 'no sé que hago con mi vida 2', 'Vota si este humilde señor deberia de comer macarrones o lentejas', '2019-11-11', '2019-11-13', 0),
 (8, 'prueba45', 'fgfdg', '2019-11-26', '2019-11-30', 0),
 (9, 'prueba50', 'gffdgsdfg', '2019-12-01', '2019-12-07', 1);
 
@@ -134,6 +145,16 @@ CREATE TABLE `voto` (
   `Id` int(32) NOT NULL,
   `Nombre` varchar(128) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `voto`
+--
+
+INSERT INTO `voto` (`Id`, `Nombre`) VALUES
+(0, 'Si'),
+(1, 'No'),
+(2, 'Blanco'),
+(3, 'Abstencion');
 
 --
 -- Índices para tablas volcadas
@@ -177,7 +198,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `votacion`
@@ -189,7 +210,7 @@ ALTER TABLE `votacion`
 -- AUTO_INCREMENT de la tabla `voto`
 --
 ALTER TABLE `voto`
-  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
