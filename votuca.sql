b610f47d9ef6cb25104ef915acba1c4a5fcc9b0f
inmabox <ibsantamaria96@gmail.com>
2019-11-14 11:28:01 +0100

    Actualizacion BD

--- votuca.sql
+++ votuca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-11-2019 a las 20:50:17
+-- Tiempo de generación: 14-11-2019 a las 10:13:31
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.2.23
 
@@ -39,8 +39,28 @@ CREATE TABLE `rol` (
 
 INSERT INTO `rol` (`Id`, `Nombre`) VALUES
 (1, 'Elector'),
-(2, 'Admin'),
-(3, 'Secretario');
+(2, 'Secretario'),
+(3, 'Secretario delegado'),
+(4, 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `secretariosDelegados`
+--
+
+CREATE TABLE `secretariosDelegados` (
+  `Id_Secretario` int(11) NOT NULL,
+  `Id_Votacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `secretariosDelegados`
+--
+
+INSERT INTO `secretariosDelegados` (`Id_Secretario`, `Id_Votacion`) VALUES
+(4, 3),
+(3, 3);
 
 -- --------------------------------------------------------
 
@@ -62,7 +82,7 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`Id`, `Id_Rol`, `NombreUsuario`, `Password`) VALUES
 (1, 1, 'u00000000', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.'),
 (2, 2, 'a00000000', '$2y$12$sZ9YHmBqYETwRKfIKGSUT.4ti4rlapaM5uYNj2M.tn21KxSGlytLG'),
-(3, 3, 's00000000', '$2y$12$IAuZ40LZiE8HQ2hZATzDau.817xDgTBW.KtJqC3avOJRbW4nTMvpK');
+(3, 3, 'u12345678', '$2y$12$aecF4Ak8JHHsEWHHoVzs7.UQ/IXMpyekhuG8vXjJ61HXy5aJ84WV.');
 
 -- --------------------------------------------------------
 
@@ -97,7 +117,12 @@ CREATE TABLE `votacion` (
 
 INSERT INTO `votacion` (`Id`, `Titulo`, `Descripcion`, `FechaInicio`, `FechaFinal`, `isDelected`) VALUES
 (1, 'Primera Votacion', 'Esta es la primera votacion de mi sistema', '2019-11-10', '2019-11-16', 0),
-(2, 'Otra Votacion', 'Soy otra votacion', '2019-11-24', '2019-11-27', 0);
+(2, 'Otra Votacion modificada', 'Soy otra votacion', '2019-11-24', '2019-11-27', 0),
+(3, 'otra jejejje ', 'fvfdbghggh', '2019-11-11', '2019-11-16', 0),
+(4, 'ME HE MODIFICADO', 'fvfdbghggh', '2019-11-11', '2019-11-17', 0),
+(7, 'no sé que hago con mi vida 2', 'Vota si este humilde señor deberia de comer macarrones o lentejas', '2019-11-11', '2019-11-13', 0),
+(8, 'prueba45', 'fgfdg', '2019-11-26', '2019-11-30', 0),
+(9, 'prueba50', 'gffdgsdfg', '2019-12-01', '2019-12-07', 1);
 
 -- --------------------------------------------------------
 
@@ -146,7 +171,7 @@ ALTER TABLE `voto`
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -158,7 +183,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `votacion`
 --
 ALTER TABLE `votacion`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `voto`
