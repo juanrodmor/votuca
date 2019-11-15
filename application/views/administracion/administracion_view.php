555b3cb1fc76eed5c956b28e8adcac4c96300d25
Juan Rodríguez <50288510+juanrodmor@users.noreply.github.com>
2019-11-15 11:34:36 +0100

    Admin views uploaded

---
+++ application/views/administracion/administracion_view.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Panel de administración</title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url(); ?>/assets/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">VotUCA</a>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <a class="nav-link active" href="#">Gestionar roles</a>
+          <a class="nav-link" href="<?= base_url().'administracion/monitoring'?>">Auditoría</a>
+        </ul>
+        <a class="nav-link text-success" href="<?= base_url().'login_controller/logout'?>">Cerrar sesión</a>
+      </div>
+
+    </nav>
+
+    <div class="container" style="margin-top: 8%;">
+  
+  <div class="alert alert-warning alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        <strong>Atención.</strong> Estás trabajando actualmente en la vista de <strong>gestión de roles</strong>.
+    </div>
+
+    <div class="jumbotron">
+        <center><h1>Administracion</h1></center>
+    </div>
+
+  </div>
+
+</div>
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url(); ?>/assets/js/jquerySlim.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
+
+    <!-- Scripts para la tabla de votaciones -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.0/js/dataTables.buttons.min.js"></script>
+    <script src="<?php echo base_url()."assets/js/behaviour/administracion_votaciones.js"?>"></script>
+
+    <!-- DATE PICKER -->
+    <script src="<?php echo base_url(); ?>/assets/js/bootstrap-datepicker.js"></script>
+
+
+  </body>
+
+
+</html>
