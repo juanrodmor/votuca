e47c76a43494b42764559d331e8a2fd42aae233b
Juan Rodríguez <50288510+juanrodmor@users.noreply.github.com>
2019-11-15 12:10:15 +0100

    Updating admin-views

--- application/views/administracion/administracionMonitoring_view.php
+++ application/views/administracion/administracionMonitoring_view.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Panel de administración</title>
+    <title>Panel de administración - Auditorías</title>
+
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>/assets/css/bootstrap-datepicker.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style> @import url(<?php echo base_url('assets/css/admin_css.css')?>); </style>
 
   </head>
 
@@ -28,18 +28,17 @@
 
     </nav>
 
-    <div class="container">
-        <div class="alert alert-warning alert-dismissible" style="margin-top:8%;">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Atención.</strong> Estás trabajando actualmente en la vista de <strong>auditorías</strong>.
-        </div>
-    </div>
-
-  <div class="container" style="margin-top: 8%;">
+  <div class="container">
   
     <div class="jumbotron">
-        <center><h1>Administracion</h1></center>
+        <center><h1>Administración</h1></center>
+        <center><h3>Auditoría</h3></center>
     </div>
+
+    <div class="card">
+        <div class="card-body">Hola</div>
+    </div>
+
 
   </div>
 
@@ -49,22 +48,13 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url(); ?>/assets/js/jquerySlim.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-    <!-- Scripts para la tabla de votaciones -->
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.0/js/dataTables.buttons.min.js"></script>
-    <script src="<?php echo base_url()."assets/js/behaviour/administracion_votaciones.js"?>"></script>
-
-    <!-- DATE PICKER -->
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap-datepicker.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
 
 
   </body>
-
+  
 
 </html>
--- application/views/administracion/administracion_view.php
+++ application/views/administracion/administracion_view.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Panel de administración</title>
+    <title>Panel de administración - Gestionar roles</title>
+
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>/assets/css/bootstrap-datepicker.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style> @import url(<?php echo base_url('assets/css/admin_css.css')?>); </style>
 
   </head>
 
@@ -28,15 +28,11 @@
 
     </nav>
 
-    <div class="container" style="margin-top: 8%;">
+    <div class="container">
   
-  <div class="alert alert-warning alert-dismissible">
-        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        <strong>Atención.</strong> Estás trabajando actualmente en la vista de <strong>gestión de roles</strong>.
-    </div>
-
     <div class="jumbotron">
-        <center><h1>Administracion</h1></center>
+        <center><h1>Administración</h1></center>
+        <center><h3>Gestión de roles</h3></center>
     </div>
 
   </div>
@@ -46,21 +42,12 @@
 
 
 
-    <!-- Bootstrap core JavaScript
+ <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url(); ?>/assets/js/jquerySlim.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap.min.js"></script>
-
-    <!-- Scripts para la tabla de votaciones -->
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.0/js/dataTables.buttons.min.js"></script>
-    <script src="<?php echo base_url()."assets/js/behaviour/administracion_votaciones.js"?>"></script>
-
-    <!-- DATE PICKER -->
-    <script src="<?php echo base_url(); ?>/assets/js/bootstrap-datepicker.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
   </body>
