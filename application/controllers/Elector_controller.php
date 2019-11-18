98c71ee5091b51cc3dfaa70cc5613244d71c9761
juancarlosld <56406256+juancarlosld@users.noreply.github.com>
2019-11-18 18:12:22 +0100

    Mostrar resultados (1er prototipo)

--- application/controllers/Elector_controller.php
+++ application/controllers/Elector_controller.php
@@ -8,18 +8,22 @@ class Elector_controller extends CI_Controller {
 	}
 	
 	public function index() {
-
-        $id_user = $this->Voto_model->_userId($_SESSION['usuario']);
-        $datos = $this->Voto_model->_listar($id_user);
-        $votos = array( 
-        	'datos' => $datos
-        );
-		$this->load->view('Elector/votacion_view', $votos);
+		if ($this->session->userdata('rol') != 'Elector') {
+			redirect('/Login_controller');
+	    }
+	    else {
+	    	$id_user = $this->Voto_model->_userId($_SESSION['usuario']);
+	        $datos = $this->Voto_model->_listar($id_user);
+	        $votos = array( 
+	        	'datos' => $datos
+	        );
+			$this->load->view('Elector/votacion_view', $votos);
+	    }     
     }
 
     public function votar($id_votacion, $titulo) {
     	$id_usuario = $this->Voto_model->_userId($_SESSION['usuario']);
-    	$votos = $this->Voto_model->_votosDisponibles();
+    	$votos = $this->Voto_model->_votosDisponibles();	// habrá que pasarle $id_votacion para mostrar los votos disponibles para esa votacion
     	$datos = array(
     		'titulo' => $titulo,
     		'id_votacion' => $id_votacion,
@@ -37,4 +41,18 @@ class Elector_controller extends CI_Controller {
     	$this->Voto_model->_votar($id_usuario, $id_votacion, $voto);
     	$this->index();
     }
+
+    public function verResultados($id_votacion, $titulo) {
+    	$datos = $this->Voto_model->recuentoVotos($id_votacion);
+    	$total = sizeof($datos);
+    	$recVotos = $this->Voto_model->tiposVotos($datos);
+
+    	$datos = array(
+    		'total' => $total,
+    		'titulo' => $titulo,
+    		'votos' => $recVotos
+    	);
+    	$this->load->view('Elector/resultados_view', $datos);
+    }
+
 }
