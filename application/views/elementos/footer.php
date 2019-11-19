efc1ba826e91632d89a502a636f06faa8aa44f6c
inmabox <ibsantamaria96@gmail.com>
2019-11-19 17:27:02 +0100

    Diseño del footer y tablas actualizado

--- application/views/elementos/footer.php
+++ application/views/elementos/footer.php
@@ -3,17 +3,17 @@
   <div class="container">
       <div class="row">
         <div class="col-sm-3">
-          <img src="<?php echo base_url('assets/img/footer.png')?>"class="img-fluid" alt="Responsive image">
+        <img src="<?php echo base_url('assets/img/footer.png')?>"class="img-fluid" alt="Responsive image">
           <!--<div class="row text-center"> © 2019. Hecho por grupo 5 pinf.</div>-->
         </div>
-        <div class="col-sm-3 col-xs-12">
+        <!--<div class="col-sm-3 col-xs-12">
           <ul class="payment">
-            <li><p>Centro cultural reina sofia</p></li>
-            <li><p>C/Paseo Carlos III nº 9</p></li>
-            <li><p>11003, Cádiz</p></li>
-            <li><p>CÁDIZ</p></li>
+            <li>Centro cultural reina sofia</li>
+            <li>C/Paseo Carlos III nº 9</li>
+            <li>11003, Cádiz</li>
+            <li>CÁDIZ</li>
           </ul>
-        </div>
+        </div>--->
       </div>
   </div>
 </footer>
