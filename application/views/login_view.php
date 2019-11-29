d466881df4011f117a573e7b2b4bee4aeee46aa2
Juan Rodríguez <50288510+juanrodmor@users.noreply.github.com>
2019-11-29 16:59:56 +0100

    Update login_view.php
    pequeña corrección

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>/assets/css/login_css_final.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>/assets/css/login_css.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 
