c874aa81596c579277f58ae949cef17b6e9ffc0b
inmabox <ibsantamaria96@gmail.com>
2019-12-16 20:21:20 +0100

    Vista login verificacion arreglada

--- application/views/login_verification.php
+++ application/views/login_verification.php
@@ -4,23 +4,21 @@
 	{
 		echo '<img src='.$QR.'>';
 	}
-	
+
 	if(isset($mensaje))
 		echo $mensaje;
-	
-	?>
-	
-	<head></head>
-	
-	<body>
-	
-		<form action="pass_auth" method="post">
-		
-			<input type="text" name="key">
-			<input type="submit" value="enviar" name="enviar"> 
-		
-		</form>
-	
-	</body>
-	
 
+	?>
+
+	<head></head>
+
+	<body>
+
+		<form action="<?php echo base_url() . 'login_controller/pass_auth';?>" method="post">
+
+			<input type="text" name="key">
+			<input type="submit" value="enviar" name="enviar">
+
+		</form>
+
+	</body>
